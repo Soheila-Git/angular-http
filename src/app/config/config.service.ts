63fbc27e755bc3011daada3acc53d55d85b12a7a
Soheila Akbarzadeh <soheila.akbarzadeh@gmail.com>
2018-07-01 15:45:40 -0700

    implement getConfigResponse() method service
    The HttpClient.get() returns a full response, an Observable of typed
    HttpResponse rather than just the JSON data.

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 export interface Config {
   heroesUrl: string;
@@ -16,5 +17,11 @@ export class ConfigService {
 
   getConfig() {
     return this.http.get<Config>(this.configUrl);
+  }
+
+  getConfigResponse(): Observable<HttpResponse<Config>> {
+    return this.http.get<Config>(
+      this.configUrl, { observe: 'response' }
+    );
   }
 }
