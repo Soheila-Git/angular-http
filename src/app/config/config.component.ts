559791a93c447be3b67346553d781f52ebd78fca
Soheila Akbarzadeh <soheila.akbarzadeh@gmail.com>
2018-06-30 17:59:15 -0700

    inject the ConfigService to the ConfigComponent

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from './config.service';
 
 @Component({
   selector: 'app-config',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigComponent implements OnInit {
 
-  constructor() { }
+  constructor(private configService: ConfigService) { }
 
   ngOnInit() {
   }
