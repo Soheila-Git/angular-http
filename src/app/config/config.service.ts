33f0d058cf0958411994b58e134f6965dbd03263
Soheila Akbarzadeh <soheila.akbarzadeh@gmail.com>
2018-07-10 00:11:16 -0700

    add handleError() method
    The handleError() method interprets the error and returns an RxJS
    ErrorObservable with a user-friendly error message.

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 
 export interface Config {
   heroesUrl: string;
@@ -24,4 +24,20 @@ export class ConfigService {
       this.configUrl, { observe: 'response' }
     );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if(error.error instanceof ErrorEvent) {
+      // A client-side or network error occured
+      console.error('An error occured: ', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);   
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened: please try again later.');
+  };
 }
